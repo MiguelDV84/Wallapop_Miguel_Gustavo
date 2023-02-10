4c1f0fd8093fdf9550e95bdb905fc7aea1ecce0d
MiguelDV84 <miguel.diazv84@gmail.com>
2023-02-10 18:03:30 +0100

    Rutas controller index.php
    Ahora todas las rutas pasan por el index.php, excepto la de subir anuncio

--- app/vistas/anuncios.php
+++ app/vistas/anuncios.php
@@ -9,52 +9,11 @@ $anuncioDAO = new AnuncioDAO(ConexionBD::conectar());
 
 // llama al método getAnuncios() a través de la instancia
 $array_anuncios = $anuncioDAO->getAnuncios();
+
+
 ?>  
 
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Catalog-Z Bootstrap 5.0 HTML Template</title>
-        <link rel="stylesheet" href="web/css/bootstrap.min.css">
-        <link rel="stylesheet" href="web/fontawesome/css/all.min.css">
-        <link rel="stylesheet" href="web/css/templatemo-style.css">
-        
-    </head>
-    <body>
-        <!-- Page Loader -->
-        <div id="loader-wrapper">
-            <div id="loader"></div>
 
-            <div class="loader-section section-left"></div>
-            <div class="loader-section section-right"></div>
-
-        </div>
-        <nav class="navbar navbar-expand-lg">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="anuncios.php">
-
-                    WallaFake
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fas fa-bars"></i>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link nav-link-1 active" aria-current="page" href="app/vistas/anuncios.php">Anuncios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link nav-link-2" href="app/vistas/subirAnuncio.php">Mis Anuncios</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link nav-link-3" href="app/vistas/login.php">Login/Registro</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
 
         <div class="tm-hero d-flex justify-content-center align-items-center" data-parallax="scroll" data-image-src="../../web/img/hero.jpg">
             <form class="d-flex tm-search-form">
@@ -106,7 +65,6 @@ $array_anuncios = $anuncioDAO->getAnuncios();
                     <div class="d-flex justify-content-between tm-text-gray">
                         <span class="tm-text-gray-light">18 Oct 2020</span>
                         <span>9,906 €</span>
-
                     </div>
                     <p>Esta sería la descripción del articulo</p>
                 </div>
--- app/vistas/registro.php
+++ app/vistas/registro.php
@@ -1,54 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Catalog-Z Contact page</title>
-    <link rel="stylesheet" href="../../web/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../../web/fontawesome/css/all.min.css">
-    <link rel="stylesheet" href="../../web/css/templatemo-style.css">
-<!--
-    
-TemplateMo 556 Catalog-Z
-
-https://templatemo.com/tm-556-catalog-z
-
--->
-</head>
-<body>
-    <!-- Page Loader -->
-    <div id="loader-wrapper">
-        <div id="loader"></div>
-
-        <div class="loader-section section-left"></div>
-        <div class="loader-section section-right"></div>
-
-    </div>
-    <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="anuncios.php">
-                
-                WallaFake
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <i class="fas fa-bars"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link nav-link-1 active" aria-current="page" href="../../index.php">Anuncios</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-2" href="subirAnuncio.php">Mis Anuncios</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link nav-link-3" href="registro.php">Login/Registro</a>
-                </li>
-            </ul>
-            </div>
-        </div>
-    </nav>
-
+<?php
+require 'app/vistas/plantilla.php';
+?>
 
 
     <div class="container-fluid tm-mt-60">
@@ -156,7 +108,7 @@ https://templatemo.com/tm-556-catalog-z
             </div>
         </div>
     </footer>    
-    <script src="../../web/js/plugins.js"></script>
+    <script src="web/js/plugins.js"></script>
     <script>
         $(window).on("load", function() {
             $('body').addClass('loaded');
